efc660e61c5ea6532503f2d6551ab4fc1ae2641e
DooSYaa <sashadubrovsky2005@gmail.com>
2025-07-09 15:56:20 +0200

    qert

--- TaskHiveApi/obj/Debug/net8.0/TaskHiveApi.AssemblyInfo.cs
+++ TaskHiveApi/obj/Debug/net8.0/TaskHiveApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskHiveApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a08d14726e068f9e9c2b248cc8db10545ec71a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8719d3785aaf6fcb2d881c532ad8ab81608746")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskHiveApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskHiveApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
